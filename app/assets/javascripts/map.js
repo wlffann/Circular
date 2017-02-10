36f8bb559dbe73cd678c9527bb2663afe39c0f51
Zack Forbing <zackforbing@gmail.com>
2017-02-09 19:24:05 -0700

    fixed ENVs

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,45 +2,45 @@ $(document).ready(function(){
   initMap();
 })
 
-
-
-// function initMap() {
-//   handler = Gmaps.build('Google');
-//   handler.buildMap({ provider: {
-//   }, internal: {id: 'map'}}, function(){
-//     markers = handler.addMarkers([
-//       {
-//         "lat": 39.7392,
-//         "lng": -104.9903,
-//         "picture": {
-//           "url": "",
-//           "width":  32,
-//           "height": 32
-//         },
-//         "infowindow": "hello!"
-//       }
-//     ]);
-//     handler.bounds.extendWith(markers);
-//     handler.fitMapToBounds();
-//     handler.getMap().setZoom(10);
-//   });
-// }
+var googleMapsSecret = $('body').data('env')
 
 function initMap() {
-  var latLng = {
-    lat: 39.7392,
-    lng: -104.9903
-  }
-  var map = new google.maps.Map($("#map"), {
-    zoom: 10,
-    center: latLng
-  })
+  handler = Gmaps.build('Google');
+  handler.buildMap({ provider: {
+  }, internal: {id: 'map'}}, function(){
+    markers = handler.addMarkers([
+      {
+        "lat": 39.7392,
+        "lng": -104.9903,
+        "picture": {
+          "url": "",
+          "width":  32,
+          "height": 32
+        },
+        "infowindow": "hello!"
+      }
+    ]);
+    handler.bounds.extendWith(markers);
+    handler.fitMapToBounds();
+    handler.getMap().setZoom(10);
+  });
 }
+
+// function initMap() {
+//   var latLng = {
+//     lat: 39.7392,
+//     lng: -104.9903
+//   }
+//   var map = new google.maps.Map($("#map"), {
+//     zoom: 10,
+//     center: latLng
+//   })
+// }
 
 function markerListener(map) {
   $("#submit").on('click', function(e) {
     $.ajax({
-      url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + $("#street_address").val().replace(/ /g , "+") + "&key=???",
+      url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + $("#street_address").val().replace(/ /g , "+") + "&key=" + googleMapsSecret,
       success: function(response) {
         var marker = new google.maps.Marker({
           position: response.results[0].geometry.location
