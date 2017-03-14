4473984656f235a223e443887240ca6cd0666b20
Scott Conner <m.scott.conner@gmail.com>
2017-03-13 19:53:36 -0600

    update calculator to show more options. Update html on apartments view.

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,15 +1,41 @@
 $(function(){
   var gallonsPerUnitPerWeek = 6.4;
-  var context = $("#calculator");
-  var unitsInput = context.find("#num-units");
-  var output1 = context.find("#64x2");
+  var relevantUpper = 10;
+  var relevantLower = 0.5;
+  var table = $("#calc-table");
+  var unitsInput = $("#num-units");
 
+  //Reduces displayed numbers to one decimal place.
+  //and removes trailing 0s
+  var truncZeros = function(num){
+    var round = num.toFixed(1);
+    if(round % 1 === 0){
+      return num.toFixed(0);
+    }
+    else if(num === 0){
+      return num.toFixed(0);
+    }
+    else{
+      return round;
+    }
+  };
+
+  //does the actual work of calculating the number of containers
   var calculate = function (numUnits, pickupsPerWeek, numGallons){
-    var result = (numUnits * gallonsPerUnitPerWeek) / (numGallons / pickupsPerWeek) ;
-    return result;
+    var result = (numUnits * gallonsPerUnitPerWeek) / (numGallons / pickupsPerWeek);
+    var rounded = truncZeros(result);
+    return rounded;
   };
 
   $("#num-units").keyup( function(){
-    output1.text(calculate(unitsInput.val(), 2, 64));
+  	table.find("td.calc-output").each(function(){
+  	  var cell = $(this);
+  	  var numUnits = Number(unitsInput.val())
+  		var numGallons = Number($(this).data("gallons"));
+  		var numPickups = Number($(this).data("pickups"));
+
+      result = calculate(numUnits, numPickups, numGallons);
+    	cell.text(result);
+  	});
   });
 });
