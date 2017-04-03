fbe9496f28c8f000943c566b305a2c68fa9c45e0
Tommasina <tommasina.miller@granicus.com>
2017-04-03 09:54:33 -0600

    WIP setting marker after address submission

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,38 +4,36 @@ $(document).ready(function(){
   addAddress();
 });
 
-function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
+var map;
+function initMap() {
+  var denver = { lat: 39.7392, lng: -104.9903 };
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
-    center: {
-      lat: 39.7392,
-      lng: -104.9903
-    }
+    center: denver
   });
 }
+
 
 function addAddress(){
-  // var geocoder = new google.maps.Geocoder();
+  var geocoder = new google.maps.Geocoder();
   $('.submit-address').on('click', function(e){
     e.preventDefault();
-    e.stopPropagation();
     console.log($('#street-address').val());
-    // codeAddress(geocoder);
-    // var address = $('#street-address').val();
+    var address = $('#street-address').val();
+    codeAddress(geocoder, address, map);
   });
 }
 
-function codeAddress(geocoder){
-  var address = $('.submit-address').val();
+function codeAddress(geocoder, address, map){
   console.log(address);
   if(address.length > 0){
-    geocoder.geocode({'address': address}, function(result, status){
-      debugger;
+    geocoder.geocode({'address': address}, function(results, status){
       if(status == google.maps.GeocoderStatus.OK){
-        map.setCenter(results[0].geometry.location);
+        // map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
           map: map,
-          position: results[0].geometry.location
+            zoom: 12,
+            center: results[0].geometry.location
         });
       } else {
         alert("Geocode was not successful because " + status);
